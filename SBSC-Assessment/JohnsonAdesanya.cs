f10e1e0453b4401bcb14e341d310f0e6b9132030
adesanyajohnson <adesanyajohnson2gmail.com>
2020-06-29 14:27:09 +0100

    HyphenSeperatedNumbers Implemented

--- SBSC-Assessment/JohnsonAdesanya.cs
+++ SBSC-Assessment/JohnsonAdesanya.cs
@@ -8,6 +8,26 @@ namespace SBSC_Assessment
 {
     public class JohnsonAdesanya
     {
-        
+        public bool hyphenSeperatedNumbers(string hyphenSeperated)
+        {
+            if (string.IsNullOrEmpty(hyphenSeperated))
+                return false;
+
+            List<string> splitString = hyphenSeperated.Split('-').ToList<string>();
+            int interval = 0, difference = 0, listLength = splitString.Count;
+
+            for (int i = 0; i < listLength; i++)
+            {
+                if (i == 0)
+                    interval = int.Parse(splitString[i + 1]) - int.Parse(splitString[i]);
+                else
+                    difference = int.Parse(splitString[i + 1]) - int.Parse(splitString[i]);
+
+                if (i != 0 && interval != difference)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
