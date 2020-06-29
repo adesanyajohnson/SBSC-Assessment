88860ce9cd5b8e9c1cdc5247db4ca96d9b57d500
adesanyajohnson <adesanyajohnson2gmail.com>
2020-06-29 15:33:20 +0100

    Question2 TestMethod Implemented

--- SBSC-Assessment-UnitTest/JohnsonAdesanyaTest.cs
+++ SBSC-Assessment-UnitTest/JohnsonAdesanyaTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SBSC_Assessment;
+using System.Collections.Generic;
 
 namespace SBSC_Assessment_UnitTest
 {
@@ -19,6 +20,11 @@ namespace SBSC_Assessment_UnitTest
         [TestMethod]
         public void Question2TestMethod()
         {
+            int expected = 7;
+            List<int> inputList = new List<int> {1, 4, 6, 9 };
+            JohnsonAdesanya assessmentClass = new JohnsonAdesanya();
+            int result = assessmentClass.Question2(inputList);
+            Assert.AreEqual(expected, result, string.Format("Question2 Method returned {0} instead of 7", result.ToString()));
         }
 
         [TestMethod]
