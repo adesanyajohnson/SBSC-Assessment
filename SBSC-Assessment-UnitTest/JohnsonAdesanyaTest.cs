51c7a80edd0343d2db107d4ec6e18e1c746cc683
adesanyajohnson <adesanyajohnson2gmail.com>
2020-06-29 15:22:59 +0100

    Added Unit Test Project

---
+++ SBSC-Assessment-UnitTest/JohnsonAdesanyaTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SBSC_Assessment;
+
+namespace SBSC_Assessment_UnitTest
+{
+    [TestClass]
+    public class JohnsonAdesanyaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
